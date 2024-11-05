328a62f7de9065abbd03360a88740a0cf946a040
Samra Nawaz <167023891+SamraNawaz@users.noreply.github.com>
2024-11-05 22:57:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+//--------------------ref section
+let ref_section = document.getElementById('ref');
+let ref_btn = document.getElementById('ref_btn');
+ref_btn?.addEventListener('click', () => {
+    ref_section.classList.toggle('hide');
+});
+//--------------------exp section-------------------------
+let exp_section = document.getElementById('experience');
+let exp_btn = document.getElementById('exp_btn');
+ref_btn?.addEventListener('click', () => {
+    ref_section.classList.toggle('hide');
+});
+//---------------------print button 
+let print_btn = document.getElementById('print_btn');
+print_btn?.addEventListener('click', () => {
+    window.print();
+});
